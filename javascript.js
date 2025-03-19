7ca980a5cd5d7289e6fcfa2ada5247e756c018b9
JamesMFox <31540421+JamesMFox@users.noreply.github.com>
2025-03-18 20:28:11 -0500

    Added basic functions; add, subtract, divide, multiply. quick and dirty;

--- javascript.js
+++ javascript.js
@@ -1,3 +1,16 @@
-function foo(bar) {
-  return bar;
+function add(a, b) {
+  return a + b;
 };
+
+function subtract(a, b) {
+  return a - b;
+};
+
+function multiply(a, b) {
+  return a * b;
+};
+
+function divide(a, b) {
+  return a / b;
+};
+
